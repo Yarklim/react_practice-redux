1defaf128c57c4d682f695d4094d7a1306a3d1d1
Yaroslav Klimas <yarklim@gmail.com>
2023-09-28 17:38:09 +0300

    added plugins

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Hello Yar</div>;
-}
-
-export default App;
---
+++ src/containers/App/App.jsx
@@ -0,0 +1,11 @@
+import s from './App.module.scss';
+
+function App() {
+  return (
+    <>
+      <h1 className={s.title}>Hello Yar</h1>
+    </>
+  );
+}
+
+export default App;
