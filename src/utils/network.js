c6985e86d312f2874750584f9385450a1ea458be
Yaroslav Klimas <yarklim@gmail.com>
2023-09-28 18:58:55 +0300

    update fetch

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,10 +1,23 @@
 const SWAPI_URL = 'https://swapi.dev/api/';
 const SWAPI_PEOPLE = 'people';
 
-export const getApiResource = (url) => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((body) => console.log(body));
+export const getApiResource = async (url) => {
+  try {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      console.error('Could not fetch: ', res.status);
+      return false;
+    }
+
+    return await res.json();
+  } catch (err) {
+    console.error('Could not fetch: ', err.message);
+    return false;
+  }
 };
 
-getApiResource(SWAPI_URL + SWAPI_PEOPLE);
+(async () => {
+  const body = await getApiResource(SWAPI_URL + SWAPI_PEOPLE);
+  console.log(body);
+})();
