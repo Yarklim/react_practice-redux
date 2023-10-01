a6ab3e852ed33957ff679d14f1a70c525d02456b
Yaroslav Klimas <yarklim@gmail.com>
2023-10-01 18:34:53 +0300

    added character card

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,19 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getApiResource } from '../../utils/network';
 import { API_PEOPLE } from '../../constants/api';
+import { getPeopleId, getPeopleImg } from '../../services/getPeopleData';
 import s from './PeoplePage.module.scss';
 
 const PeoplePage = () => {
+  const [people, setPeople] = useState(null);
+
   const getResource = async (url) => {
-    const body = await getApiResource(url);
-    console.log(body);
+    const res = await getApiResource(url);
+
+    const peopleList = res.results.map(({ name, url }) => {
+      const id = getPeopleId(url);
+      const img = getPeopleImg(id);
+
+      //   console.log(img);
+
+      return {
+        id,
+        name,
+        img,
+      };
+    });
+
+    setPeople(peopleList);
   };
 
   useEffect(() => {
     getResource(API_PEOPLE);
   }, []);
 
-  return <>PeoplePage</>;
+  return (
+    <>
+      <ul>
+        {people?.map(({ id, name, img }) => (
+          <li className={s.card} key={id}>
+            <img src={img} alt={name} />
+            <p>{name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default PeoplePage;
