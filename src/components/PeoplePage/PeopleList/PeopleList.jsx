9586ba626141517c61b7d27e9c4d0e8cfe282658
Yaroslav Klimas <yarklim@gmail.com>
2023-10-02 18:33:11 +0300

    added styles for PeopleList

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -3,11 +3,13 @@ import s from './PeopleList.module.scss';
 
 const PeopleList = ({ people }) => {
   return (
-    <ul>
+    <ul className={s.listContainer}>
       {people?.map(({ id, name, img }) => (
-        <li className={s.card} key={id}>
-          <img src={img} alt={name} />
-          <p>{name}</p>
+        <li className={s.listItem} key={id}>
+          <a className={s.listLink} href="#">
+            <img className={s.personPhoto} src={img} alt={name} />
+            <p className={s.itemTitle}>{name}</p>
+          </a>
         </li>
       ))}
     </ul>
